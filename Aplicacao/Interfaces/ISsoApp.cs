97732e04da951a64366d4f9da78235a2b9436cb0
Saulo Gustavo <70772825+Saulo0100@users.noreply.github.com>
2026-03-24 12:49:18 -0300

    Removido metodo não utilizado

--- Aplicacao/Interfaces/ISsoApp.cs
+++ Aplicacao/Interfaces/ISsoApp.cs
@@ -1,10 +1,7 @@
-﻿using SsoBarbearia.Modelos.Response;
-
-namespace SsoBarbearia.Aplicacao.Interfaces
+﻿namespace SsoBarbearia.Aplicacao.Interfaces
 {
     public interface ISsoApp
     {
-        ConfiguracaoSiteResponse ObterConfiguracao(string dominio);
         bool VerificarDominio(string dominio);
     }
 }
--- Controllers/SsoController.cs
+++ Controllers/SsoController.cs
@@ -6,12 +6,6 @@ namespace SsoBarbearia.Controllers
     public class SsoController(ISsoApp app) : ControllerBase
     {
         private readonly ISsoApp _app = app;
-        [HttpGet]
-        public IActionResult ObterConfiguracao([FromQuery] string dominio)
-        {
-            var resultado = _app.ObterConfiguracao(dominio);
-            return Ok(resultado);
-        }
 
         [HttpGet("verificar-dominio")]
         public IActionResult VerificarDominio([FromQuery] string dominio)
